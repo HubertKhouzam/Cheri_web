8935da45dc015dfc2da380a5396bc6d60d5ff193
Kevin He <ipman4567@gmail.com>
2023-10-28 20:58:44 -0400

    Initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import Navigation from '../Components/Navbar';
+
+function Home() {
+  return (
+    <div className='tamere'>
+        <Navigation />
+        <div className='cheri'> 
+          <h1> Chéri</h1>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
